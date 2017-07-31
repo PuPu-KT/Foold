0ac7bf3f38bced085cc70af96f4bfcabd4b03974
tiffchuasx <tiffany.chua@icloud.com>
2017-07-31 14:55:55 +0800

    Edited Header & Footer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_many :reviews
+  has_many :reviews
 
 end
