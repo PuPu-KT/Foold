a8b22885821cd0fd74d74345c238340ca5195345
tiffchuasx <tiffany.chua@icloud.com>
2017-08-02 11:31:28 +0800

    restaurant and review rendering fixed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,7 +10,7 @@ class Restaurant < ActiveRecord::Base
   geocoded_by :full_address
   after_validation :geocode
 
-  mount_uploader :image, ImageUploader
+  # mount_uploader :image, ImageUploader
 
   def full_address
      [address1, address2, city, state, zipcode].join(', ')
